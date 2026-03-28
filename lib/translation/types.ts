fc099207b39fbf90b8e03dbce8b6d0ece8e85bae
kevin <kevin.lam.1013@gmail.com>
2026-03-28 19:47:47 -0400

    unused file/folder + refactoring

--- lib/translation/types.ts
+++
@@ -1,9 +0,0 @@
-export type LanguageCode = 'en-US' | 'fr-FR' | 'es-ES' | 'zh-CN';
-
-export interface TrackDescriptor {
-  speakerId: string;
-  language: LanguageCode;
-  trackLabel: string; // e.g., `${speakerId}:${language}`
-}
-
-export type PipelineState = 'starting' | 'active' | 'stopping' | 'stopped' | 'error';
